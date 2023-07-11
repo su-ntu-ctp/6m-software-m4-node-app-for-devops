60422bb06ee363c84e67510efdfc1d9cc6acd305
jieromu <jerome.leong@gmail.com>
2023-07-11 20:16:58 +0800

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello world!");
+    res.send("Hello world!!");
 }
 
 module.exports = print;
